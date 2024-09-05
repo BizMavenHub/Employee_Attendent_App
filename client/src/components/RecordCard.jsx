cd89b12a33ecd38de39dd19f45d56293181142d4
TepChoulong <tepchoulongzin@gmail.com>
2024-09-05 18:09:24 +0700

    add recordCard component and handle login

--- client/src/components/RecordCard.jsx
+++ client/src/components/RecordCard.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import moment from "moment";
+
 import "../styles/components/recordCard.css";
 
-const RecordCard = () => {
-  return <div>RecordCard</div>;
+const RecordCard = ({ loginTime, logoutTime }) => {
+  const calculateDuration = () => {
+    let Duration = 0;
+    const loginTimeToHour = new Date(loginTime).getTime() / 1000 / 60 / 60;
+    const logoutTimeToHour = new Date(logoutTime).getTime() / 1000 / 60 / 60;
+    Duration = logoutTimeToHour - loginTimeToHour;
+    return Duration;
+  };
+
+  return (
+    <div className="record-card">
+      <div>
+        <p>Date: {moment(loginTime).format("DD/MM/YYYY")}</p>
+        <p>Login: {moment(loginTime).format("hh:mm A")}</p>
+        <p>
+          {logoutTime
+            ? `Logout: ${moment(logoutTime).format("hh:mm A")}`
+            : "Logout: Not Yet Available"}
+        </p>
+      </div>
+      <div>
+        <p>
+          {loginTime && logoutTime
+            ? `Duration: ${calculateDuration().toFixed(2)}`
+            : "Duration: Not Yet Available"}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default RecordCard;
--- client/src/pages/Home_Page.jsx
+++ client/src/pages/Home_Page.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { logoutSuccess } from "../redux/user/userSlice";
-import moment from "moment";
+
 import "../styles/home_page.css";
+
 import Narbar from "../components/Navbar";
 import Heading from "../components/Heading";
+import RecordCard from "../components/RecordCard";
 
 const Home_Page = () => {
   const currentUser = useSelector((state) => state.user.currentUser);
@@ -14,6 +15,39 @@ const Home_Page = () => {
   const dispatch = useDispatch();
 
   const [cookies, setCookie, removeCookie] = useCookies(["access_token"]);
+
+  const [userTimeLogs, setUserTimeLogs] = useState([]);
+
+  async function getUserTimeLogs() {
+    try {
+      const res = await fetch(
+        `http://localhost:3000/timeLog/${currentUser._id}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.message);
+      }
+
+      setUserTimeLogs(data.timeLog);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getUserTimeLogs();
+
+    console.log(userTimeLogs);
+  }, [cookies.access_token]);
 
   return (
     <>
@@ -24,7 +58,15 @@ const Home_Page = () => {
       ) : (
         <div className="home_page_logged_in_container">
           <Heading />
-          <div className="main_container"></div>
+          <div className="main_container">
+            {userTimeLogs.map((timeLog, index) => (
+              <RecordCard
+                key={index}
+                loginTime={timeLog.loginTime}
+                logoutTime={timeLog.logoutTime}
+              />
+            ))}
+          </div>
           <Narbar />
         </div>
       )}
