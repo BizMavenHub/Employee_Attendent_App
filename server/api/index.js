dd144f01438fd0ce0d6606bff23b40849ad87dfe
TepChoulong <tepchoulongzin@gmail.com>
2024-09-09 13:13:01 +0700

    update use Cors

--- server/api/index.js
+++ server/api/index.js
@@ -23,6 +23,12 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 connectDB(); // Connect to MongoDB
 
 app.use("/api/auth", authRouter);
