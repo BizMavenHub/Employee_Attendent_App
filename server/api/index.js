cea2d82823ff46b64dc7d4b7465ac2331dff5fb5
TepChoulong <tepchoulongzin@gmail.com>
2024-09-11 11:45:28 +0700

    Fox CORS blocked

--- server/api/index.js
+++ server/api/index.js
@@ -14,19 +14,26 @@ dotenv.config();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL, // Allow all origins
-    credentials: true,
+    origin: "https://employee-attendent-app.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://employee-attendent-app.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
+
+app.options("*", cors());
 
 connectDB(); // Connect to MongoDB
 
